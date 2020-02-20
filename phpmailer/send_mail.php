f187175ad37903bbc1d50ba4ad9dd119b084239e
Sikandar <sikandarali31@gmail.com>
2020-02-20 15:35:16 +0500

    new php_mailer

---
+++ phpmailer/send_mail.php
@@ -0,0 +1,45 @@
+<?php
+$mailto = 'sikandarali31@gmail.com';
+
+$mailSub = $_POST['email'];
+$mailMsg = $_POST['message'];
+$name = $_POST['first-name'];
+$p_num = $_POST['phone'];
+
+
+   require 'PHPMailer-master/PHPMailerAutoload.php';
+   $mail = new PHPMailer();
+   $mail ->IsSmtp();
+   $mail ->SMTPDebug = 0;
+   $mail ->SMTPAuth = true;
+   $mail ->SMTPSecure = 'ssl';
+   $mail ->Host = "smtp.gmail.com";
+   $mail ->Port = 465; // or 587
+   $mail ->IsHTML(true);
+   $mail ->Username = "sikandarali31@gmail.com  ";
+   $mail ->Password = "sikandar";
+   $mail ->SetFrom("sikandarali31@gmail.com");
+
+   $mail ->Subject = $mailSub.$name;
+   $mail ->Body = $name." <br>".$p_num." <br>".$mailSub." <br>".$mailMsg;
+   $mail ->AddAddress($mailto);
+
+   if(!$mail->Send())
+   {
+       echo "Mail Not Sent";
+   }
+   else
+   {
+       echo '<script language="javascript">';
+       echo 'alert("Information has been saved ")';
+       echo '</script>';
+       echo "<script> location='/razaaq/contact.php' </script>";
+   }
+
+?>
+<h1 style="margin-top: 50px"></h1>
+
+
+
+   
+
